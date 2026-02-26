6ccf18e6f01583be7fb34758b867c5f8a497f6aa
CPN\jpinto <jgpb_1987@hotmail.com>
2026-02-26 18:52:53 -0500

    Se agregan archivos

---
+++ gad.aaportal.commons/Dto/Log/Log.cs
@@ -0,0 +1,42 @@
+﻿using gad.aaportal.commons.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gad.aaportal.commons.Dto.Log
+{
+    public class LogParam
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string CodigoUsuario { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public long Secuencial { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string NumeroDocumento { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public DateTime FechaHoraEnvio { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public DateTime FechaHoraRespuesta { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string Proveedor { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string Metodo { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string TramaEnvio { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string TramaRespuesta { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string Codigo { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public string Mensaje { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio para realizar la transacción")]
+        public bool Exito { get; set; }
+    }
+    public class LogResult : BaseResult
+    {
+        public long IdInsercion { get; set; }
+    }
+}
---
+++ gad.aaportal.models/Entity/Log/SolicitudRespuesta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gad.aaportal.models.Entity.Log
+{
+    public partial class SolicitudRespuesta
+    {
+        public long Id { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string CodigoUsuario { get; set; } = null!;
+        public long Secuencial { get; set; }
+        public string NumeroDocumento { get; set; } = null!;
+        public DateTime FechaHoraEnvio { get; set; }
+        public DateTime FechaHoraRespuesta { get; set; }
+        public string Proveedor { get; set; } = null!;
+        public string Metodo { get; set; } = null!;
+        public string TramaEnvio { get; set; } = null!;
+        public string TramaRespuesta { get; set; } = null!;
+        public string Codigo { get; set; } = null!;
+        public string Mensaje { get; set; } = null!;
+        public bool Exito { get; set; }
+    }
+}

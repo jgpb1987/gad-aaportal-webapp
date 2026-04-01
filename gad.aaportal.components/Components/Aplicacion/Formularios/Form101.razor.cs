a73de16c8731b35c4323dc503888b54981911f0f
Cfcabrera <christaian.cabrera@gmail.com>
2026-04-01 10:27:10 -0500

    Sesion CI quemada

--- gad.aaportal.components/Components/Aplicacion/Formularios/Form101.razor.cs
+++ gad.aaportal.components/Components/Aplicacion/Formularios/Form101.razor.cs
@@ -10,9 +10,8 @@ namespace gad.aaportal.components.Components.Aplicacion.Formularios
 {
     public partial class Form101 : ComponentBase
     {
-        //private string ruc = "1002001749001";//SE DEBE TOMAR EL VALOR DE SESION
-        private string ruc = "1091730940001";//SE DEBE TOMAR EL VALOR DE SESION
-        private string tipoPersona = "PJ";//SE DEBE TOMAR EL VALOR DE SESION
+        private string ruc = "1002346649001";//SE DEBE TOMAR EL VALOR DE SESION
+        private string tipoPersona = "PN";//SE DEBE TOMAR EL VALOR DE SESION
 
         private string? razSocial { get; set; }
         private List<int> anios = new();

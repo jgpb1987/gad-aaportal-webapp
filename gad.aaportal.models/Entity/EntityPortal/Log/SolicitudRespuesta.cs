ab4c5ab6d272a5cf5ea9c93a0be49ff285bb16ed
Jose Pinto Barreto <jgpb_1987@hotmail.com>
2026-04-16 22:37:14 -0500

    ok

---
+++ gad.aaportal.models/Entity/EntityPortal/Log/SolicitudRespuesta.cs
@@ -0,0 +1,20 @@
+﻿namespace gad.aaportal.models.Entity.Log
+{
+    public partial class SolicitudRespuesta
+    {
+        public long Id { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string CodigoUsuario { get; set; } = null!;
+        public long Secuencial { get; set; }
+        public string NumeroDocumento { get; set; } = null!;
+        public DateTime FechaHoraEnvio { get; set; }
+        public DateTime FechaHoraRespuesta { get; set; }
+        public string Proveedor { get; set; } = null!;
+        public string Metodo { get; set; } = null!;
+        public string TramaEnvio { get; set; } = null!;
+        public string TramaRespuesta { get; set; } = null!;
+        public string Codigo { get; set; } = null!;
+        public string Mensaje { get; set; } = null!;
+        public bool Exito { get; set; }
+    }
+}
